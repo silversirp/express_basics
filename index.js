9396dc524ba294b979daa576a7ff5832dfc6f4ee
Silver Sirp <silver.sirp@khk.ee>
2022-02-03 11:22:34 +0200

    Basic use of ejs

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 let express = require('express');
 const app = express();
 
-app.get('/', (req, res)=> {
-    res.send('<a href="/contact">Contact Us</a> <br> <a href="/about">About Us</a>');
-});
-
-app.get('/contact', (req, res)=> {
-    res.send('<h1>Contact Us Page</h1>');
-});
-
-app.get('/about', (req, res)=> {
-    res.send('<h1>About Us Page</h1>');
-});
-
-app.get('*', (req, res)=> {
-    res.send('404. This page does not exist. <a href="/">Go To HomePage</a>');
+app.get('/demo', (req, res)=> {
+    res.render('index.ejs');
 });
 
 app.listen(3000, ()=> {
