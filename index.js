201069714c11eff1e8859e6cdeff12dcd95da5a1
Silver Sirp <silver.sirp@khk.ee>
2022-02-10 01:22:46 +0200

    template loop use

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ app.set('views', path.join(__dirname, 'views'));
 //add views template engine
 app.set('view engine', 'ejs');
 
-app.get('/user/:username', (req, res)=> {
-    let user = req.params.username;
-    res.render('index.ejs', {username: user});
+app.get('/questions', (req, res)=> {
+    let questions = [
+        {title: "What is Node.js?", user: "Kadi", votes: "10"},
+        {title: "What is Express.js?", user: "Mine", votes: "8"}
+    ]
+    res.render('index.ejs', {questions:questions});
 });
 
 app.listen(3000, ()=> {
