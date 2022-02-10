55ed9c3b7910e09a4f54c5622410cdf50ed8a593
Silver Sirp <silver.sirp@khk.ee>
2022-02-10 09:55:31 +0200

    post request use

--- index.js
+++ index.js
@@ -2,20 +2,35 @@ let express = require('express');
 //require path
 const path = require('path');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 // add public directory
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+
 //add views directory path
 app.set('views', path.join(__dirname, 'views'));
 //add views template engine
 app.set('view engine', 'ejs');
 
-app.get('/questions', (req, res)=> {
-    let questions = [
-        {title: "What is Node.js?", user: "Kadi", votes: "10"},
-        {title: "What is Express.js?", user: "Mine", votes: "8"}
-    ]
-    res.render('index.ejs', {questions:questions});
+app.get('/login', (req, res)=> {
+
+    res.render('login');
 });
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if(username == 'user' && password == 'qwerty') {
+        res.redirect('/dashboard');
+    }
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
 
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000');
